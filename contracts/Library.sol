be89d9410323fdf641e8a70e6f7946ffb80c0f4b
grc02 <georgi.chonkov27@gmail.com>
2022-05-24 00:15:35 +0300

    Added a returnBook func

--- contracts/Library.sol
+++ contracts/Library.sol
@@ -12,7 +12,7 @@ contract Library {
    }
 
    Book[] public bookList;
-   mapping (address => uint[]) ownersOfBooks;
+   mapping (address => uint) ownersOfBooks;
 
    function addBook(bytes32 bookName, bytes32 author, uint yearOfPublishment) external {
       bookList.push(Book(bookList.length, bookName, author, yearOfPublishment, true));
@@ -20,11 +20,22 @@ contract Library {
 
    // Check if the requested book is available, by not being read by someone else, if so -> add it to the list of owner's list of books
    function getBook(bytes32 _bookName, bytes32 _author) external {
+      require(ownersOfBooks[msg.sender] != 0, "You are already reading, finish and return the current one before you take another!");
       for(uint i; i < bookList.length; i++) {
          if(bookList[i].bookName == _bookName && bookList[i].author == _author) {
             bookList[i].available = false;
-            ownersOfBooks[msg.sender].push(bookList[i].id);
+            ownersOfBooks[msg.sender] = bookList[i].id;
          }
       }
    }
+
+   function returnBook() external {
+      for(uint i; i < bookList.length; i++) {
+         if(bookList[i].id == ownersOfBooks[msg.sender]) {
+            bookList[i].available = true;
+         }
+      }
+      delete ownersOfBooks[msg.sender];
+   }
+
 }
